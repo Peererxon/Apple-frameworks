f73c7d0125b73d7da53c7115012e42b9c62afbc8
andersongil <agilp@copaair.com>
2024-05-25 10:01:22 -0400

    Initial Commit

---
+++ Apple-frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple-frameworks
+//
+//  Created by Anderson Gil on 25/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
